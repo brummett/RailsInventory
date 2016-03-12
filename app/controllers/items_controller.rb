4f6f4ab0c4604a85e5da05ea594962775c39913f
Anthony Brummett <brummett@gmail.com>
2016-03-12 12:42:46 -0600

    Create a new item and show it

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
+    def show
+        @item = Item.find(params[:id])
+    end
+
     def new
+    end
+
+    def create
+        @item = Item.new(item_params)
+        @item.save
+        redirect_to @item
+    end
+
+    private def item_params
+        params.require(:item).permit(:name, :sku, :barcode, :cost_cents, :price_cents, :description)
     end
 end
