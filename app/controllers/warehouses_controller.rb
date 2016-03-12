8e401d1ab401808a7c362a3de020a42850515c62
Anthony Brummett <brummett@gmail.com>
2016-03-12 16:12:19 -0600

    CRUD for warehouses

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,2 +1,47 @@
 class WarehousesController < ApplicationController
+    def index
+        @warehouses = Warehouse.all
+    end
+
+    def show
+        @warehouse = Warehouse.find(params[:id])
+    end
+
+    def new
+        @warehouse = Warehouse.new
+    end
+
+    def edit
+        @warehouse = Warehouse.find(params[:id])
+    end
+
+    def create
+        @warehouse = Warehouse.new(warehouse_params)
+        if @warehouse.save
+            redirect_to @warehouse
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @warehouse = Warehouse.find(params[:id])
+
+        if @warehouse.update(warehouse_params)
+            redirect_to @warehouse
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @warehouse = Warehouse.find(params[:id])
+        @warehouse.destroy
+
+        redirect_to warehouses_path
+    end
+
+    private def warehouse_params
+        params.require(:warehouse).permit(:name, :address, :phonenumber, :email, :notes)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
     resources :items
+    resources :warehouses
 
   # You can have the root of your site routed with "root"
   root 'home#index'
