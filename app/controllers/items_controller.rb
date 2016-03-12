a342418a7d8f8c66314f539cfc6fe77fb724f5e5
Anthony Brummett <brummett@gmail.com>
2016-03-12 15:23:00 -0600

    allow editing an item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,12 +11,26 @@ class ItemsController < ApplicationController
         @item = Item.new
     end
 
+    def edit
+        @item = Item.find(params[:id])
+    end
+
     def create
         @item = Item.new(item_params)
         if @item.save
             redirect_to @item
         else
             render 'new'
+        end
+    end
+
+    def update
+        @item = Item.find(params[:id])
+
+        if @item.update(item_params)
+            redirect_to @item
+        else
+            render 'edit'
         end
     end
 
